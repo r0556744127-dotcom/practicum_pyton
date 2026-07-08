b51e9ca661a476a2b5e73c42c3c0a769551b63df
רחל ליפשיץ <r0556744127@gmail.com>
2026-07-08 17:32:00 +0300

    iteration4

--- ChessGame.py
+++ ChessGame.py
@@ -11,37 +11,32 @@ class ChessGame:
     def handle_click(self, x: int, y: int) -> None:
         coords = self._board.convert_coordinates(x, y)
         if coords is None:
-            return  # לחיצה מחוץ לגבולות הלוח - מתעלמים
+            return
 
         row, col = coords
 
         if self._selected_cell is None:
-            # שלב הבחירה: בוחרים כלי רק אם המשבצת אינה ריקה
             if not self._board.is_empty_cell(row, col):
                 self._selected_cell = (row, col)
         else:
-            # שלב ביצוע המהלך
             start_row, start_col = self._selected_cell
             
-            # אם לחצו על כלי ידידותי אחר - מחליפים את הבחירה לכלי החדש
+            # אם השחקן לוחץ על כלי אחר מאותו הצבע, נחליף את הבחירה לכלי החדש
             if self._board.is_friendly_piece(row, col, self._selected_cell):
                 self._selected_cell = (row, col)
                 return
 
-            # שליפת סוג הכלי (לדוגמה 'wK' -> לוקחים את 'K')
-            piece_token = self._board.get_piece_at(start_row, start_col)
-            piece_type = piece_token[1]
-
-            # בדיקת חוקיות התנועה באמצעות הפונקציה הגיאומטרית
-            if Piece.is_legal_move(piece_type, start_row, start_col, row, col):
-                # ביצוע המהלך ועדכון הלוח
+            # בדיקת חוקיות המהלך המלאה (כולל חוסמים ואכילה)
+            if Piece.is_legal_move(self._board, start_row, start_col, row, col):
+                piece_token = self._board.get_piece_at(start_row, start_col)
+                
+                # ביצוע המהלך (אם היה שם כלי אויב, הוא פשוט נדרס/נאכל)
                 self._board.set_piece_at(row, col, piece_token)
                 self._board.set_piece_at(start_row, start_col, '.')
-                self._selected_cell = None  # איפוס הבחירה לאחר מהלך מוצלח
-            else:
-                # מהלך לא חוקי - מתעלמים ומבטלים את הבחירה הנוכחית
                 self._selected_cell = None
-
+            else:
+                # מהלך לא חוקי - מבטלים את הבחירה
+                self._selected_cell = None
     def handle_wait(self, ms: int) -> None:
         """מטפל בפקודת המתנה."""
         pass
--- Piece.py
+++ Piece.py
@@ -8,22 +8,63 @@ class Piece:
         return token[0] in ['w', 'b'] and token[1] in ['K', 'Q', 'R', 'B', 'N', 'P']
 
     @staticmethod
-    def is_legal_move(piece_type: str, start_row: int, start_col: int, end_row: int, end_col: int) -> bool:
+    def is_legal_move(board, start_row: int, start_col: int, end_row: int, end_col: int) -> bool:
+        """
+        בודקת חוקיות מהלך כולל חוסמים בדרך ואכילת כלי אויב.
+        """
+        # 1. עמידה במקום אינה מהלך חוקי
         if start_row == end_row and start_col == end_col:
             return False
 
+        # 2. שליפת הכלים בנקודת ההתחלה והסוף
+        moving_piece = board.get_piece_at(start_row, start_col)
+        target_piece = board.get_piece_at(end_row, end_col)
+        
+        if moving_piece == '.':
+            return False
+            
+        piece_type = moving_piece[1]
+        moving_color = moving_piece[0]
+
+        # 3. בדיקה אם יעד התנועה מכיל כלי מאותו הצבע
+        if target_piece != '.' and target_piece[0] == moving_color:
+            return False
+
+        # חישוב מרחקים
         dr = abs(end_row - start_row)
         dc = abs(end_col - start_col)
-        
-        if piece_type == 'K':      
+
+        # 4. בדיקת דפוס תנועה גיאומטרי וחוסמים בדרך לפי סוג הכלי
+        if piece_type == 'K':      # מלך
             return dr <= 1 and dc <= 1
-        elif piece_type == 'R':   
-            return dr == 0 or dc == 0
-        elif piece_type == 'B':   
-            return dr == dc
-        elif piece_type == 'Q':   
-            return dr == 0 or dc == 0 or dr == dc
-        elif piece_type == 'N':    
-            return (dr == 2 and dc == 1) or (dr == 1 and dc == 2)
             
+        elif piece_type == 'N':    # פרש (יכול לקפוץ מעל חוסמים, לכן אין בדיקת מסלול)
+            return (dr == 2 and dc == 1) or (dr == 1 and dc == 2)
+
+        # עבור כלים שנעים למרחק (צריח, רץ, מלכה) - נבדוק חסימות בדרך
+        elif piece_type in ['R', 'B', 'Q']:
+            # בדיקת התאמה גיאומטרית בסיסית
+            if piece_type == 'R' and not (dr == 0 or dc == 0):
+                return False
+            if piece_type == 'B' and not (dr == dc):
+                return False
+            if piece_type == 'Q' and not (dr == 0 or dc == 0 or dr == dc):
+                return False
+
+            # קביעת כיוון הצעדים (1, 0, או 1-)
+            step_row = 0 if end_row == start_row else (1 if end_row > start_row else -1)
+            step_col = 0 if end_col == start_col else (1 if end_col > start_col else -1)
+
+            # סריקת המסלול (לא כולל נקודת ההתחלה ונקודת הסוף)
+            current_row = start_row + step_row
+            current_col = start_col + step_col
+
+            while current_row != end_row or current_col != end_col:
+                if board.get_piece_at(current_row, current_col) != '.':
+                    return False  # נמצא חוסם בדרך!
+                current_row += step_row
+                current_col += step_col
+
+            return True
+
         return False
